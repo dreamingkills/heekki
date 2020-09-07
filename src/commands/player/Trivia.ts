2e1803699074deee3b52adc6a7103ac6dcc305b3
olivia-hye <realrtfl@gmail.com>
2020-09-06 23:46:29 -0400

    bug proofing

--- src/commands/player/Trivia.ts
+++ src/commands/player/Trivia.ts
@@ -52,7 +52,7 @@ export class Command extends GameCommand {
           `<:red_x:741454361007357993> You didn't get the answer in time. :confused:`
         );
       }
-      if (msg.deletable) channel.bulkDelete(collected);
+      if (msg && msg.deletable) channel.bulkDelete(collected);
     });
   };
 }
