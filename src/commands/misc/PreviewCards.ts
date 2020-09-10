b50c146a7d45c8129c39af34284f20c4bd3762ca
olivia-hye <realrtfl@gmail.com>
2020-09-10 01:46:29 -0400

    add forSale to card previews

--- src/commands/misc/PreviewCards.ts
+++ src/commands/misc/PreviewCards.ts
@@ -7,6 +7,7 @@ import {
   TextChannel,
 } from "discord.js";
 import { CardService } from "../../database/service/CardService";
+import { MarketService } from "../../database/service/MarketService";
 
 export class Command extends GameCommand {
   names: string[] = ["preview"];
@@ -15,7 +16,7 @@ export class Command extends GameCommand {
   category: string = "player";
 
   exec = async (msg: Message) => {
-    const references = this.prm.filter((p) => p.includes("#"));
+    const references = this.prm.filter((p) => p.includes("#")).slice(0, 9);
     const cardList = await Promise.all(
       references.map(async (p) => {
         return (
@@ -29,15 +30,24 @@ export class Command extends GameCommand {
 
     const embed = new MessageEmbed()
       .setAuthor(
-        `Card previews | ${msg.author.tag}`,
+        `Card Previews | ${msg.author.tag}`,
         msg.author.displayAvatarURL()
       )
       .setDescription(`**${cardList.length}** cards requested...`)
       .setColor(`#40BD66`);
-    for (let card of cardList.slice(0, 9)) {
+    for (let card of cardList) {
+      const isInMarketplace = await MarketService.cardIsOnMarketplace(
+        card.userCardId
+      );
       embed.addField(
         `${card.abbreviation}#${card.serialNumber}`,
-        `**${card.title}**\n${card.member}\n:star: **${card.stars}**\n:heart: **${card.hearts}**\nOwner: <@${card.ownerId}>`,
+        `**${card.title}**\n${card.member}\n:star: **${
+          card.stars
+        }**\n:heart: **${card.hearts}**\nOwner: <@${card.ownerId}>${
+          isInMarketplace.forSale
+            ? `\n:dollar: For Sale: **${isInMarketplace.price}** <:coin:745447920072917093>`
+            : ""
+        }`,
         true
       );
     }
