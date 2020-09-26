afa55032083f5ced7f0c8731ccb271b5d37fe246
olivia-hye <realrtfl@gmail.com>
2020-09-26 18:47:00 -0400

    fix

--- src/commands/debug/Guilds.ts
+++ src/commands/debug/Guilds.ts
@@ -7,15 +7,8 @@ export class Command extends BaseCommand {
   users: string[] = ["197186779843919877"];
   exec = async (msg: Message) => {
     const desc = msg.client.guilds.cache.map((g) => {
-      return `__**${g.name}**__\n**${g.memberCount}** members\n**Owner**: <@${
-        g.ownerID
-      }>\n<${g.iconURL()}>\n`;
+      return `__**${g.name}**__\n**${g.memberCount}** members\n**Owner**: <@${g.ownerID}>\n`;
     });
-    const desc1 = desc.slice(0, 1900);
-    const desc2 = desc.slice(1900, 3800);
-    const desc3 = desc.slice(3800, 5700);
-    await msg.channel.send(desc1);
-    await msg.channel.send(desc2);
-    await msg.channel.send(desc3);
+    await msg.channel.send(desc);
   };
 }
