5d3b475e2f8c92cbbdaaf9280b57ba6d1b67af8d
olivia-hye <realrtfl@gmail.com>
2020-10-01 09:22:30 -0400

    fix heart send cd

--- src/commands/player/FriendHearts.ts
+++ src/commands/player/FriendHearts.ts
@@ -4,10 +4,16 @@ import { BaseCommand } from "../../structures/command/Command";
 import { Profile } from "../../structures/player/Profile";
 import Chance from "chance";
 import { PlayerService } from "../../database/service/PlayerService";
+import * as error from "../../structures/Error";
 
 export class Command extends BaseCommand {
   names: string[] = ["send"];
   exec = async (msg: Message, executor: Profile) => {
+    const last = await PlayerService.getLastHeartSend(executor);
+    const now = Date.now();
+    if (now < last + 3600000) {
+      throw new error.SendHeartsCooldownError(last + 3600000, now);
+    }
     const friendsRaw = await FriendService.getFriendsByProfile(executor);
     const friends = friendsRaw.map((f) => {
       return f.sender === msg.author.id ? f.recipient : f.sender;
@@ -20,6 +26,7 @@ export class Command extends BaseCommand {
       return;
     }
     await FriendService.sendHearts(executor, friends);
+    PlayerService.setLastHeartSend(executor, now);
     const chance = new Chance();
     const xp = chance.integer({ min: 40, max: 85 });
     PlayerService.addXp(executor, xp);
