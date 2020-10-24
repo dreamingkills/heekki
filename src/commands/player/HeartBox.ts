11f9e497306d5e92803839b3fad5011ac43b1967
olivia-hye <realrtfl@gmail.com>
2020-10-24 13:58:30 -0400

    remove xp from !hb

--- src/commands/player/HeartBox.ts
+++ src/commands/player/HeartBox.ts
@@ -10,15 +10,15 @@ export class Command extends BaseCommand {
     const hb = await PlayerService.openHeartBoxes(executor);
 
     const chance = new Chance();
-    const xp = chance.integer({ min: 30, max: 65 });
-    PlayerService.addXp(executor, xp);
+    //const xp = chance.integer({ min: 30, max: 65 });
+    //PlayerService.addXp(executor, xp);
     const embed = new MessageEmbed()
       .setAuthor(
         `Heart Boxes | ${msg.author.tag}`,
         msg.author.displayAvatarURL()
       )
       .setDescription(
-        `:gift_heart: You opened some heart boxes and received **${hb.added}** <:heekki_heart:757147742383505488>\n+ **${xp}** XP`
+        `:gift_heart: You opened some heart boxes and received **${hb.added}** <:heekki_heart:757147742383505488>` //\n+ **${xp}** XP`
       )
       .setFooter(
         `You now have ${hb.total} hearts.\nYou can open heart boxes again in 4 hours.`
