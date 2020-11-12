94229443ae101f6fe9ec396d231068eb27e168c8
olivia-hye <realrtfl@gmail.com>
2020-11-11 22:02:37 -0500

    add koxx easter egg to ping

--- src/commands/debug/Ping.ts
+++ src/commands/debug/Ping.ts
@@ -4,7 +4,9 @@ import { Message } from "discord.js";
 export class Command extends BaseCommand {
   names: string[] = ["ping", "pong"];
   async exec(msg: Message): Promise<void> {
-    await msg.channel.send("Hello!");
+    await msg.channel.send(
+      `${this.config.discord.emoji.check.full} **ACCESS: OK**\nhttps://open.spotify.com/album/1QaUqc2Guft8ZUCW5mphtE`
+    );
     return;
   }
 }
