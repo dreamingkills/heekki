6a46d4a4e6efb92f898326a60b58d624747ce9bf
olivia-hye <realrtfl@gmail.com>
2021-06-19 14:37:45 -0400

    bonus

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { Bot } from "./structures/client/Bot";
 import { DB } from "./database/index";
 import { FontLoader } from "./helpers/FontLoader";
-import { main } from "./api/index";
+// import { main } from "./api/index";
 
-Promise.all([DB.connect(), FontLoader.init(), main()]).then(() => {
+Promise.all([DB.connect(), FontLoader.init() /*main()*/]).then(() => {
   new Bot().init();
 });
